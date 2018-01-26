06c12444322c83b2f2328040389f12366355f42c
flabers <toostupidmonkey@gmail.com>
2018-01-26 17:44:50 +0200

    ll

--- src/assets/components/FirstComponent.vue
+++ src/assets/components/FirstComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
       <h2>I`m ready to start master</h2>
+      <button class="btn btn-primary" v-on:click="show = !show">Click and give me life</button>
+      <transition name="fade">
+          <p v-if="show"><br>I`m free</p>
+      </transition>
   </div>
 </template>
 
@@ -15,10 +19,23 @@ var myMixin = {
 }
 
 export default {
-      mixins: [myMixin],
+    mixins: [myMixin],
+    data() {
+        return {
+            show: false
+        }
+    },
     methods: {
-
     },
 
 }
 </script>
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
 
 import BooComponent from './assets/components/BooComponent.vue'
 import FooComponent from './assets/components/FooComponent.vue'
